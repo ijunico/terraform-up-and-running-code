c8402bf6f50145124dc32fdb2a00a426a8da6b97
Inna Kholodova <inna.kholodova@gmail.com>
2021-02-20 22:36:01 +0400

    fixing version constraints

--- code/terraform/00-preface/hello-world/main.tf
+++ code/terraform/00-preface/hello-world/main.tf
@@ -1,12 +1,13 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.12, < 0.15"
 }
 
 provider "aws" {
   region = "us-east-2"
 
   # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
+  # this (version) constraints should be moved to required_providers block
+  # version = "~> 2.0"
 }
 
 resource "aws_instance" "example" {
